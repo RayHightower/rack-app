72a87ded7b157914e9d5d0959179e82df96e7769
Raymond T. Hightower <rth@wisdomgroup.com>
2017-07-28 21:52:03 -0500

    Cleanup.

--- simple_echo_server.rb
+++ simple_echo_server.rb
@@ -4,7 +4,7 @@ require 'rack'
 
 app = Proc.new do |env|
   echo_string = env["QUERY_STRING"][4..-1]
-  ['200', {'Content-Type' => 'text/html'}, ["#{echo_string}\n\n"]]
+  ['200', {'Content-Type' => 'text/html'}, ["#{echo_string}"]]
 end
 
 Rack::Handler::WEBrick.run app
