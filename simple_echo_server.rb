d156b634da3ae54627e8363ab9e3cbccd10ad794
Raymond T. Hightower <rth@wisdomgroup.com>
2017-07-28 21:27:46 -0500

    Create first version of the simple echo server.

--- simple_echo_server.rb
+++ simple_echo_server.rb
@@ -3,7 +3,7 @@
 require 'rack'
 
 app = Proc.new do |env|
-  ['200', {'Content-Type' => 'text/html'}, ["A barebones rack app. env = #{env}"]]
+  ['200', {'Content-Type' => 'text/html'}, ["#{env["QUERY_STRING"]}"]]
 end
 
 Rack::Handler::WEBrick.run app
