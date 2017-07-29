cbe3965f65fc1f151eba3cd4adf98f2de5df84eb
Raymond T. Hightower <rth@wisdomgroup.com>
2017-07-28 21:45:51 -0500

    Get that slice working on the string, okay?

--- fun_env.rb
+++ fun_env.rb
@@ -1,9 +1,10 @@
-# my_rack_app.rb
+# fun_env.rb
 
 require 'rack'
 
 app = Proc.new do |env|
-  ['200', {'Content-Type' => 'text/html'}, ["env = #{env}"]]
+  echo_string = env["QUERY_STRING"].slice(3)
+  ['200', {'Content-Type' => 'text/html'}, ["#{echo_string}\n\n"]]
 end
 
 Rack::Handler::WEBrick.run app
