bba5e2109bdb97833386e9b96eb9b2c6a4f9d638
Raymond T. Hightower <rth@wisdomgroup.com>
2017-07-28 21:21:05 -0500

    Create stub for simple_echo_server.

--- simple_echo_server.rb
+++ simple_echo_server.rb
@@ -3,7 +3,7 @@
 require 'rack'
 
 app = Proc.new do |env|
-  ['200', {'Content-Type' => 'text/html'}, ["A barebones rack app."]]
+  ['200', {'Content-Type' => 'text/html'}, ["A barebones rack app. env = #{env}"]]
 end
 
 Rack::Handler::WEBrick.run app
