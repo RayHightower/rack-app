bad4d333a9f71020e15a125078951dc1dd6e16c9
Raymond T. Hightower <rth@wisdomgroup.com>
2017-08-05 19:53:49 -0500

    If you use a counter, increment the counter, dude!

--- simple_echo_server.rb
+++ simple_echo_server.rb
@@ -5,6 +5,7 @@ require 'rack'
 number_of_requests = 0
 
 app = Proc.new do |env|
+  number_of_requests = number_of_requests + 1
   puts "\n\nRequest # #{number_of_requests}:\n#{env}" # This should appear on the console
   echo_string = env["QUERY_STRING"][4..-1]
   ['200', {'Content-Type' => 'text/html'}, ["#{echo_string}"]]
